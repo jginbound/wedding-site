56ac4c35c8d0ad3e988b47be74ee7e9e017298c5
Empty Arsenal <matt.merkes@gmail.com>
2014-05-04 10:52:59 -0700

    Fixed RSVP no option

--- js/controllers/addController.js
+++ js/controllers/addController.js
@@ -12,8 +12,16 @@ App.AddController = Ember.ObjectController.extend({
 				bbq	   = +this.get('bbq') || 0,
 				veggie = +this.get('veggie') || 0;
 
-			if((guests + 1) != (salmon + bbq + veggie)) {
-				console.log(guests + ': ' + salmon + ' ' + bbq + ' ' + veggie);
+			if(this.get('isComing') === false) {
+				var newRSVP = this.store.createRecord('rsvp', this.get('model'));
+				newRSVP.save();
+
+				newRSVP.one('didCreate', this, function() {
+					//L.marker([ location.lat, location.lng ]).addTo(App.map);
+					alert('Your RSVP has successfully been saved!');
+					this.transitionToRoute('rsvps');
+				});
+			} else if((guests + 1) != (salmon + bbq + veggie)) {
 				return alert('The number of guests must match the number of dinner choices.');
 			} else if (!this.get('association')) {
 				return alert('Please specify your association with the couple.')
